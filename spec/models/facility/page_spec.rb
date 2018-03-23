d6e23cfbbdc284da15dee35a0d1620567cb0c064
NAKANO Hideo <nakano@web-tips.co.jp>
2018-03-23 18:03:10 +0900

    [fix] facility/image and facility/map factories

--- spec/models/facility/page_spec.rb
+++ spec/models/facility/page_spec.rb
@@ -10,13 +10,13 @@ describe Facility::Image do
   describe "#attributes" do
     subject(:item) { model.last }
 
-    it { expect(item.becomes_with_route).not_to eq nil }
-    it { expect(item.dirname).not_to eq nil }
-    it { expect(item.basename).not_to eq nil }
-    it { expect(item.path).not_to eq nil }
-    it { expect(item.url).not_to eq nil }
-    it { expect(item.full_url).not_to eq nil }
-    it { expect(item.parent).to eq nil }
+    it { expect(item.becomes_with_route).not_to be_nil }
+    it { expect(item.dirname).to be_nil }
+    it { expect(item.basename).not_to be_nil }
+    it { expect(item.path).not_to be_nil }
+    it { expect(item.url).not_to be_nil }
+    it { expect(item.full_url).not_to be_nil }
+    it { expect(item.parent).to be_falsey }
   end
 end
 
@@ -30,12 +30,12 @@ describe Facility::Map do
   describe "#attributes" do
     subject(:item) { model.last }
 
-    it { expect(item.becomes_with_route).not_to eq nil }
-    it { expect(item.dirname).not_to eq nil }
-    it { expect(item.basename).not_to eq nil }
-    it { expect(item.path).not_to eq nil }
-    it { expect(item.url).not_to eq nil }
-    it { expect(item.full_url).not_to eq nil }
-    it { expect(item.parent).to eq nil }
+    it { expect(item.becomes_with_route).not_to be_nil }
+    it { expect(item.dirname).to be_nil }
+    it { expect(item.basename).not_to be_nil }
+    it { expect(item.path).not_to be_nil }
+    it { expect(item.url).not_to be_nil }
+    it { expect(item.full_url).not_to be_nil }
+    it { expect(item.parent).to be_falsey }
   end
 end
