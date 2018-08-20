805a629d9746390ce89aaf793a9fb8f12315f771
NAKANO Hideo <nakano@web-tips.co.jp>
2018-08-20 18:27:41 +0900

    [add] title to views and its specs

--- spec/controllers/static_pages_controller_spec.rb
+++ spec/controllers/static_pages_controller_spec.rb
@@ -1,10 +1,14 @@
 require 'spec_helper'
 
 describe StaticPagesController, type: :controller do
+  render_views
+
   describe "#home" do
     it "should get home" do
       get :home
       expect(response.status).to eq(200)
+      puts page.html
+      response.should have_selector("title", text: "Home | Ruby on Rails Tutorial Sample App")
     end
   end
 
@@ -12,6 +16,7 @@ describe StaticPagesController, type: :controller do
     it "should get help" do
       get :help
       expect(response.status).to eq(200)
+      expect(page).to have_selector("title", text: "Help | Ruby on Rails Tutorial Sample App")
     end
   end
 
@@ -19,6 +24,7 @@ describe StaticPagesController, type: :controller do
     it "should get about" do
       get :about
       expect(response.status).to eq(200)
+      expect(page).to have_selector("title", text: "About | Ruby on Rails Tutorial Sample App")
     end
   end
 end
