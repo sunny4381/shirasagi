4cdf9a49410ba8c1a5562ea742a9f0f5c36d2b52
NAKANO Hideo <nakano@web-tips.co.jp>
2018-08-21 18:17:38 +0900

    [modify] change spec type to feature

--- spec/features/static_pages_spec.rb
+++ spec/features/static_pages_spec.rb
@@ -1,29 +1,26 @@
 require 'spec_helper'
 
-describe StaticPagesController, type: :controller do
-  render_views
-
+describe StaticPagesController, type: :feature do
   describe "#home" do
     it "should get home" do
-      get :home
-      expect(response.status).to eq(200)
-      puts page.html
-      response.should have_selector("title", text: "Home | Ruby on Rails Tutorial Sample App")
+      visit static_pages_home_path
+      expect(status_code).to eq(200)
+      expect(page).to have_selector("title", text: "Home | Ruby on Rails Tutorial Sample App")
     end
   end
 
   describe "#help" do
     it "should get help" do
-      get :help
-      expect(response.status).to eq(200)
+      visit static_pages_help_path
+      expect(status_code).to eq(200)
       expect(page).to have_selector("title", text: "Help | Ruby on Rails Tutorial Sample App")
     end
   end
 
   describe "#about" do
     it "should get about" do
-      get :about
-      expect(response.status).to eq(200)
+      visit static_pages_about_path
+      expect(status_code).to eq(200)
       expect(page).to have_selector("title", text: "About | Ruby on Rails Tutorial Sample App")
     end
   end
