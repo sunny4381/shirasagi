c712ac695e54be8a4ca50c82be1728d085403b5c
NAKANO Hideo <pinarello.marvel@gmail.com>
2019-11-09 15:34:04 +0900

    [add] multiple entry points support

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,30 @@
 const path = require("path");
 const WebpackAssetsManifest = require("webpack-assets-manifest");
+const glob = require("glob");
 
 const { NODE_ENV } = process.env;
 const isProd = NODE_ENV === "production";
 
+const entries = {};
+glob.sync("app/javascript/*.js").forEach(filePath => {
+  const name = path.basename(filePath, path.extname(filePath));
+  entries[name] = path.resolve(__dirname, filePath);
+});
+
 module.exports = {
   mode: isProd ? "production" : "development",
   devtool: "source-map",
-  entry: {
-    application: path.resolve(__dirname, "app/javascript/application.js")
-  },
+  entry: entries,
   output: {
     path: path.resolve(__dirname, "public/packs"),
     publicPath: isProd ? "/packs/" : "//localhost:8081/packs/",
     filename: isProd ? "[name]-[hash].js" : "[name].js"
+  },
+  optimization: {
+    splitChunks: {
+      name: "vendor",
+      chunks: "initial"
+    }
   },
   resolve: {
     extensions: [".js"]
