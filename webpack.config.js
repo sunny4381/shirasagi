f2209e61df0883022ebad98ec5ed7711f56695c1
NAKANO Hideo <pinarello.marvel@gmail.com>
2019-11-09 15:36:19 +0900

    [add] babel support

--- webpack.config.js
+++ webpack.config.js
@@ -32,5 +32,19 @@ module.exports = {
       "Access-Control-Allow-Origin": "*"
     }
   },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"]
+          }
+        }
+      }
+    ]
+  },
   plugins: [new WebpackAssetsManifest({ publicPath: true, writeToDisk: true })]
 };
