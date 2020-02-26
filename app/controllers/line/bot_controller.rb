78e5b9be0448a82ff0ee0354b5d7554413615e67
HiraiY <hirai@web-tips.co.jp>
2020-02-26 17:45:18 +0900

    [add] added location information

--- app/controllers/line/bot_controller.rb
+++ app/controllers/line/bot_controller.rb
@@ -38,6 +38,8 @@ class Line::BotController < ApplicationController
           rescue
             answer(event)
           end
+        when Line::Bot::Event::MessageType::Location
+          client.reply_message(event['replyToken'], carousel)
         end
       end
     }
@@ -46,10 +48,14 @@ class Line::BotController < ApplicationController
 
   private
 
+  def site_id
+    Cms::Site.find_by_domain(request_host).id
+  end
+
   def phrase(event)
     Chat::Intent.find_by({phrase: event.message['text']})
   end
-  
+
   def res(event)
     template =
         {
@@ -62,17 +68,29 @@ class Line::BotController < ApplicationController
   end
 
   def answer(event)
-    if event.message['text'].eql?('yes')
-      client.reply_message(event['replyToken'], {
-          "type": "text",
-          "text": Chat::Node::Bot.first.chat_success.gsub(%r{</?[^>]+?>},'')
-      })
-    elsif event.message['text'].eql?('no')
-      client.reply_message(event['replyToken'], {
-          "type": "text",
-          "text": Chat::Node::Bot.first.chat_retry.gsub(%r{</?[^>]+?>},'')
-      })
-    else
+    begin
+      if event.message['text'].eql?('yes')
+        client.reply_message(event['replyToken'], {
+            "type": "text",
+            "text": Chat::Node::Bot.find_by(site_id: site_id).chat_success.gsub(%r{</?[^>]+?>},'')
+        })
+      elsif event.message['text'].eql?('no')
+        client.reply_message(event['replyToken'], {
+            "type": "text",
+            "text": Chat::Node::Bot.find_by(site_id: site_id).chat_retry.gsub(%r{</?[^>]+?>},'')
+        })
+      elsif event.message['text'].eql?('近くの施設を探す')
+        set_location(event)
+      elsif Facility::Node::Page.find_by({name: event.message['text']}).present?
+        client.reply_message(event['replyToken'], {
+            "type": "location",
+            "title": event.message['text'],
+            "address": Facility::Node::Page.find_by({name: event.message['text']}).address,
+            "latitude": Facility::Map.find_by(name: event.message['text']).map_points[0][:loc][0],
+            "longitude": Facility::Map.find_by(name: event.message['text']).map_points[0][:loc][1]
+        })
+      end
+    rescue
       template = []
       template << no_match << site_search(event)
       client.reply_message(event['replyToken'], template)
@@ -85,7 +103,7 @@ class Line::BotController < ApplicationController
         "altText": "this is a confirm template",
         "template": {
             "type": "confirm",
-            "text": Chat::Node::Bot.first.question.gsub(%r{</?[^>]+?>},''),
+            "text": Chat::Node::Bot.find_by(site_id: site_id).question.gsub(%r{</?[^>]+?>},''),
             "actions": [
                 {
                     "type": "message",
@@ -118,7 +136,7 @@ class Line::BotController < ApplicationController
                 {
                     "type": "uri",
                     "label": "サイト内検索結果へ移動",
-                    "uri": "https://" + Cms::Site.find(site).domains[0] + url
+                    "uri": "https://" + Cms::Site.find(site).domains[1] + url
                 }
             ]
         }
@@ -129,7 +147,7 @@ class Line::BotController < ApplicationController
   def no_match
     {
         "type": "text",
-        "text": Chat::Node::Bot.first.exception_text.gsub(%r{</?[^>]+?>},'')
+        "text": Chat::Node::Bot.find_by(site_id: site_id).exception_text.gsub(%r{</?[^>]+?>},'')
     }
   end
 
@@ -138,7 +156,7 @@ class Line::BotController < ApplicationController
       if phrase(event).suggest.present? && phrase(event).response.present?
         phrase(event).response.gsub(%r{</?[^>]+?>},'')
       else
-        Chat::Node::Bot.first.response_template.gsub(%r{</?[^>]+?>},'')
+        Chat::Node::Bot.find_by(site_id: site_id).response_template.gsub(%r{</?[^>]+?>},'')
       end
     else
       "選択肢#{templates.length + 1}"
@@ -148,7 +166,6 @@ class Line::BotController < ApplicationController
   def suggests(event)
     suggests = phrase(event).suggest
     actions = suggests.each_slice(4).to_a
-
     action_templates = []
     suggest_templates = []
     actions.each do |action|
@@ -187,7 +204,7 @@ class Line::BotController < ApplicationController
       if phrase(event).response.scan(/<p(?: .+?)?>.*?<\/p>/).present?
         phrase(event).response.scan(/<p(?: .+?)?>.*?<\/p>/).join("").gsub(%r{</?[^>]+?>},'')
       else
-        Chat::Node::Bot.first.response_template.gsub(%r{</?[^>]+?>},'')
+        Chat::Node::Bot.find_by(site_id: site_id).response_template.gsub(%r{</?[^>]+?>},'')
       end
     else
       "選択肢#{templates.length + 1}"
@@ -228,5 +245,66 @@ class Line::BotController < ApplicationController
     templates << site_search(event) if phrase(event).site_search == 'enabled'
     templates << question if phrase(event).question == 'enabled'
     templates
+  end
+
+  def set_location(event)
+    client.reply_message(event['replyToken'], {
+        "type": "template",
+        "altText": "位置検索中",
+        "template": {
+            "type": "buttons",
+            "text": "現在の位置を送信しますか？",
+            "actions": [
+                {
+                    "type": "uri",
+                    "label": "位置を送る",
+                    "uri": "line://nv/location"
+                }
+            ]
+        }
+    })
+  end
+
+  def carousel
+    facilities =  Facility::Node::Page.order_by(id: "desc").to_a
+    columns = []
+    domain = Cms::Site.find_by_domain(request_host).domains[1]
+    facilities.each do |facility|
+      facility_url = Facility::Node::Page.find_by(name: facility.name).url
+      column =
+          {
+              "title": facility.name,
+              "text": facility.address,
+              "defaultAction": {
+                  "type": "uri",
+                  "label": "View detail",
+                  "uri": "http://example.com/page/123"
+              },
+              "actions": [
+                  {
+                      "type": "message",
+                      "label": "地図",
+                      "text": facility.name
+                  },
+                  {
+                      "type": "uri",
+                      "label": "詳細情報",
+                      "uri": "https://" + domain + facility_url
+                  }
+              ]
+          }
+      columns << column
+    end
+    template = {
+        "type": "template",
+        "altText": "this is a carousel template",
+        "template": {
+            "type": "carousel",
+            "columns": columns,
+            "imageAspectRatio": "rectangle",
+            "imageSize": "cover"
+        }
+    }
+    template
   end
 end
